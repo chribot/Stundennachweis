3a0cf56096b399139d7aacbda9fcdb7028bc432c
chribot <chribot.git@gmail.com>
2022-04-20 20:13:24 +0200

    Buttons um Monate zu wechseln

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@
 function generateInputElements(id, numberOfInputs) {
     const container = document.getElementById(id);
     let html = '';
-    for (let i = 0; i < numberOfInputs; i++) {
-        html += '<input type="text">';
+    for (let i = 0; i < 31; i++) {
+        if (i < numberOfInputs) {
+            html += '<input type="text">';
+        } else {
+            html += '<input type="text" disabled>';
+        }
     }
     container.innerHTML = html;
 }
@@ -21,28 +25,37 @@ function generateInputElements(id, numberOfInputs) {
 function fillInputElements(id,
                                numberOfInputs, defaultValue,
                                arrayWeekend, arrayHoliday) {
-    const inputFelder = document.getElementById(id).childNodes;
+    const /**HTMLInputElement*/inputFelder = document.getElementById(id).childNodes;
     const hgFarbe = "rgb(244,242,239)";
     let day = 1;
-    for (const inputFeld of inputFelder) {
-        if (arrayWeekend.includes(day)) {
-            if (id==="bemerkung") {
-                inputFeld.value = "Wochenende";
+    for (let i = 0; i < 31; i++) {
+        if (i < numberOfInputs) {
+            inputFelder[i].disabled = false;
+            if (arrayWeekend.includes(day)) {
+                if (id==="bemerkung") {
+                    inputFelder[i].value = "Wochenende";
+                } else {
+                    inputFelder[i].style.backgroundColor = hgFarbe;
+                    inputFelder[i].value = "---";
+                }
+            } else if (arrayHoliday.includes(day)) {
+                if (id==="bemerkung") {
+                    inputFelder[i].value = "Feiertag";
+                } else {
+                    inputFelder[i].style.backgroundColor = hgFarbe;
+                    inputFelder[i].value = "---";
+                }
             } else {
-                inputFeld.style.backgroundColor = hgFarbe;
-                inputFeld.value = "---";
+                inputFelder[i].value = defaultValue;
+                inputFelder[i].style.backgroundColor = "transparent";
             }
-        } else if (arrayHoliday.includes(day)) {
-            if (id==="bemerkung") {
-                inputFeld.value = "Feiertag";
-            } else {
-                inputFeld.style.backgroundColor = hgFarbe;
-                inputFeld.value = "---";
-            }
+            day++;
         } else {
-            inputFeld.value = defaultValue;
+            // alle anderen Felder im Monat zurücksetzen
+            inputFelder[i].value = '';
+            inputFelder[i].disabled = true;
+            inputFelder[i].style.backgroundColor = "transparent";
         }
-        day++;
     }
 }
 
@@ -92,8 +105,42 @@ function weekendDays(month, year) {
     return wd;
 }
 
-function changeMonth() {
+function nextMonth() {
+    // Monat um 1 erhöhen
+    if (config.monat < 12) {
+        config.monat += 1;
+    } else {
+        config.monat = 1;
+        config.jahr += 1;
+    }
+    updateInputs();
+}
 
+function prevMonth() {
+    // Monat um 1 verringern
+    if (config.monat > 1) {
+        config.monat -= 1;
+    } else {
+        config.monat = 12;
+        config.jahr -= 1;
+    }
+    updateInputs();
+}
+
+function updateInputs() {
+    // Daten neu berechnen
+    const zeitraum = twoDigits(config.monat) + ' / ' + config.jahr.toString();
+    const tageMonat = endOfMonth(config.monat, config.jahr);
+    const wochenenden = weekendDays(config.monat, config.jahr);
+    const feiertage = beweglicheFeiertage(config.monat, config.jahr)
+    feiertage.concat( unbeweglicheFeiertage(config.monat, config.bundesland) );
+
+    // Werte der Inputs ändern
+    document.getElementById("zeitraum").value = zeitraum;
+    fillInputElements("arbeitsbeginn", tageMonat, config.arbeitBeginn, wochenenden, feiertage);
+    fillInputElements("arbeitsende", tageMonat, config.arbeitEnde, wochenenden, feiertage);
+    fillInputElements("zeitstunden", tageMonat, config.diffStunden, wochenenden, feiertage);
+    fillInputElements("bemerkung", tageMonat, '', wochenenden, feiertage);
 }
 
 let config = {
@@ -112,18 +159,29 @@ function loadConfigFile() {
     // config.json Datei laden
     fetch("./config.json")
         .then(response => { return response.json(); })
-        .then(/** JSON */data => { config = data; createInputs(); })
+        .then(data => {
+            // alle Daten aus der config.json
+            config = data;
+
+            // zusätzliche Daten hinzufügen
+            config.arbeitBeginn = twoDigits(config.stundeBeginn) + ':' + twoDigits(config.minuteBeginn) + ' Uhr';
+            config.arbeitEnde = config.stundeEnde + ':' + config.minuteEnde + ' Uhr';
+            const startDate = new Date(0, 0, 0, config.stundeBeginn, config.minuteBeginn, 0);
+            const endDate = new Date(0, 0, 0, config.stundeEnde, config.minuteEnde, 0);
+            const diff = endDate.getTime() - startDate.getTime();
+            config.diffStunden = Number(diff / 3600000).toString();
+
+            // Inputs erstellen
+            createInputs(); })
         .catch((error) => console.error('Error:', error));
 }
 
 function createInputs() {
-    const arbeitBeginn = twoDigits(config.stundeBeginn) + ':' + twoDigits(config.minuteBeginn) + ' Uhr';
-    const arbeitEnde = config.stundeEnde + ':' + config.minuteEnde + ' Uhr';
     const zeitraum = twoDigits(config.monat) + ' / ' + config.jahr.toString();
     const tageMonat = endOfMonth(config.monat, config.jahr);
     const wochenenden = weekendDays(config.monat, config.jahr);
-    const bl = config.bundesland; // BB oder BE
-    const feiertage = beweglicheFeiertage(config.monat, config.jahr).concat( unbeweglicheFeiertage(config.monat, bl) );
+    const feiertage = beweglicheFeiertage(config.monat, config.jahr);
+    feiertage.concat( unbeweglicheFeiertage(config.monat, config.bundesland) );
 
     // Werte in die 4 Kopffelder eintragen
     document.getElementById("teilnehmer").value = config.teilnehmer;
@@ -133,19 +191,15 @@ function createInputs() {
 
     // Arbeitsbeginn
     generateInputElements("arbeitsbeginn", tageMonat);
-    fillInputElements("arbeitsbeginn", tageMonat, arbeitBeginn, wochenenden, feiertage);
+    fillInputElements("arbeitsbeginn", tageMonat, config.arbeitBeginn, wochenenden, feiertage);
 
     // Arbeitsende
     generateInputElements("arbeitsende", tageMonat);
-    fillInputElements("arbeitsende", tageMonat, arbeitEnde, wochenenden, feiertage);
+    fillInputElements("arbeitsende", tageMonat, config.arbeitEnde, wochenenden, feiertage);
 
     // Zeitstunden
-    const startDate = new Date(0, 0, 0, config.stundeBeginn, config.minuteBeginn, 0);
-    const endDate = new Date(0, 0, 0, config.stundeEnde, config.minuteEnde, 0);
-    const diff = endDate.getTime() - startDate.getTime();
-    const diffStunden = diff / 3600000;
     generateInputElements("zeitstunden", tageMonat);
-    fillInputElements("zeitstunden", tageMonat, diffStunden.toString(), wochenenden, feiertage);
+    fillInputElements("zeitstunden", tageMonat, config.diffStunden, wochenenden, feiertage);
 
     // Bemerkung
     generateInputElements("bemerkung", tageMonat);
